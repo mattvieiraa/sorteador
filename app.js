a23df38f234562ad8441aea6398b33c34a054f90
Mateus Vieira <mateus_vieira1@hotmail.com>
2025-05-09 19:58:39 -0300

    SemBotaoReiniciarAinda

--- app.js
+++ app.js
@@ -8,12 +8,26 @@ function sortear(){
 
     for (let i = 0; i < quantidade; i++) {
         numero = obterNunumeroAleatorio(de, ate);
+        while(sorteados.includes(numero)) {
+            numero = obterNunumeroAleatorio(de, ate);
+        }
         sorteados.push(numero);
     }
 
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
+//    alterarBotao();
 }
+//    function alterarBotao() {
+//        
+//    }
 
-function obterNunumeroAleatorio(min, max){
+    function obterNunumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+    function novoJogo(){
+    numero = gerarNumeroAleatorio();
+    limparCampo();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+ }
